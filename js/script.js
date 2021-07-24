b116e46fd43cb8e226b7a323aaf8d4d9f183f55e
Leonardo <leonardo.otero390@gmail.com>
2021-07-24 13:11:12 -0300

    V5 - Send buttom gets green when items are selected

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
-function selectMealItem(optionMealSelected){
+
+function isItReady() {
+    if (verifyMeal !== null) {
+        if (verifyDrink !== null) {
+            if (verifyDessert !== null) {
+                const sendReady = document.querySelector(".send-request");
+                sendReady.classList.add("send-ready");
+                sendReady.innerHTML = "Fechar pedido";
+            }
+        }
+    }
+}
+function selectMealItem(optionMealSelected) {
     let verify = document.querySelector(".meal .selected");
-    if (verify !== null){
+    if (verify !== null) {
         verify.classList.remove("selected");
     }
-     optionMealSelected.classList.toggle("selected");
+    optionMealSelected.classList.toggle("selected");
+    verifyMeal = document.querySelector(".meal .selected");
+    isItReady();
 }
-function selectDrinkItem(optionDrinkSelected){
+function selectDrinkItem(optionDrinkSelected) {
     let verify = document.querySelector(".drink .selected");
-    if (verify !== null){
+    if (verify !== null) {
         verify.classList.remove("selected");
     }
-     optionDrinkSelected.classList.toggle("selected");
+    optionDrinkSelected.classList.toggle("selected");
+    verifyDrink = document.querySelector(".drink .selected");
+    isItReady();
 }
-function selectDessertItem(optionDessertSelected){
+function selectDessertItem(optionDessertSelected) {
     let verify = document.querySelector(".dessert .selected");
-    if (verify !== null){
+    if (verify !== null) {
         verify.classList.remove("selected");
     }
-     optionDessertSelected.classList.toggle("selected");
+    optionDessertSelected.classList.toggle("selected");
+    verifyDessert = document.querySelector(".dessert .selected");
+    isItReady();
 }
