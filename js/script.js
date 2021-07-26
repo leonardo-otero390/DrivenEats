444ae90249c46e864d62bbd632cbc4dc26d70474
Leonardo <leonardo.otero390@gmail.com>
2021-07-26 15:54:16 -0300

    V9 - confirmation screen workin, project done

--- js/script.js
+++ js/script.js
@@ -48,9 +48,8 @@ function selectDessertItem(optionDessertSelected) {
     isItReady();
 }
 function sendMessage(total) {
-    if (readyToAsk === true) {
-        confirmRequest();
-        let message = `Olá, gostaria de fazer o pedido:
+    confirmRequest();
+    let message = `Olá, gostaria de fazer o pedido:
         - Prato: ${mealName}
         - Bebida: ${drinkName}
         - Sobremesa: ${dessertName}
@@ -58,12 +57,47 @@ function sendMessage(total) {
         
         Nome: ${clientName}
         Endereço: ${clientAddress}`;
-        console.log(message);
-        let URI = "https://wa.me/5567992727452?text=" + encodeURIComponent(message);
-        window.open(URI)
-    }
+    console.log(message);
+    let URI = "https://wa.me/5567992727452?text=" + encodeURIComponent(message);
+    window.open(URI)
 }
 function confirmRequest() {
     clientName = prompt("Diga seu nome pu favoh");
     clientAddress = prompt("Onde cê táah?");
+}
+function reviewText() {
+    let stringMealPrice = mealPrice.toFixed(2).toString().replace(".", ",");
+    let stringDrinkPrice = drinkPrice.toFixed(2).toString().replace(".", ",");
+    let stringDessertPrice = dessertPrice.toFixed(2).toString().replace(".", ",");
+    document.querySelector(".review").innerHTML= `
+    <div class="meal">
+        <h2>${mealName}</h2>
+        <h2>${stringMealPrice}</h2>
+    </div>
+    <div class="drink">
+        <h2>${drinkName}</h2>
+        <h2>${stringDrinkPrice}</h2>
+    </div>
+    <div class="dessert">
+        <h2>${dessertName}</h2>
+        <h2>${stringDessertPrice}</h2>
+    </div>
+    <div class="total">
+        <h2> <strong>TOTAL</strong></h2>
+        <h2> <strong>R$ ${total}</strong></h2>
+    </div>`;
+
+}
+function showConfirmScreen() {
+    reviewText();
+    if (readyToAsk === true) {
+
+        let element = document.querySelector(".hidden");
+        element.classList.remove("hidden");
+
+    }
+}
+function hideConfirmScreen() {
+    let element = document.querySelector(".confirm-screen");
+    element.classList.add("hidden");
 }
